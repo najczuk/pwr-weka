69055f0b4495bfe7a9ea0105ba8c869157583640
Adrian Najczuk <najczuk@gmail.com>
2014-05-19 21:13:51 +0200

    GainRatioAttrEval Demo

--- src/GainRatioAttrEval.java
+++ src/GainRatioAttrEval.java
@@ -16,7 +16,9 @@ public class GainRatioAttrEval {
     Attribute evaluatedAttr, classAttr;
     int[][] classAttrFreqMatrix;
     int evaluatedAttrDistinctValues, classAttrDistinctValues;
-    double logBase = 2.0;
+    double[] attributeProbability, classProbability;
+    double[][] conditionalClassProbability;
+    double logBase = 2;
 
     public GainRatioAttrEval(Instances data, String evaluatedAttr, String classAttr) {
         this.data = data;
@@ -24,10 +26,24 @@ public class GainRatioAttrEval {
         this.classAttr = data.attribute(classAttr);
         this.evaluatedAttrDistinctValues = data.numDistinctValues(this.evaluatedAttr);
         this.classAttrDistinctValues = data.numClasses();
-        initializeClassAttrFreqMatrix();
+
+        classAttrFreqMatrix = initializeClassAttrFreqMatrix();
+        classProbability = calculateProbabilityForClasses();
+        attributeProbability = calculateProbabilityForAttributes();
+        conditionalClassProbability = calculateConditionalProbabilityForClasses();
+//        System.out.println("hAttr: " + hAttribute());
+//        System.out.println("hClass: " + hClass());
+//        System.out.println("hClassAttr: " + hClassAttribute());
+//        System.out.println((hClass() - hClassAttribute()) / hAttribute());
+
     }
 
-    private void initializeClassAttrFreqMatrix() {
+    public double gain() {
+        System.out.println(toString());
+        return Double.isNaN((hClass() - hClassAttribute()) / hAttribute()) ? 0 :(hClass() - hClassAttribute()) / hAttribute();
+    }
+
+    private int[][] initializeClassAttrFreqMatrix() {
         evaluatedAttrDistinctValues = evaluatedAttr.numValues();
         classAttrDistinctValues = classAttr.numValues();
         classAttrFreqMatrix = new int[classAttrDistinctValues][evaluatedAttrDistinctValues];
@@ -36,15 +52,65 @@ public class GainRatioAttrEval {
             classAttrFreqMatrix[(int) data.instance(instanceNum).value(classAttr)]
                     [(int) data.instance(instanceNum).value(evaluatedAttr)]++;
         }
+//        System.out.println(Arrays.deepToString(classAttrFreqMatrix));
+        return classAttrFreqMatrix;
 
 
     }
 
-    private double[] calculateConditionalProbabilityForClasses(){
-        double[] conditionalProbabilityForClasses = new double[classAttrDistinctValues];
-        for (int classIndex = 0; classIndex < classAttrDistinctValues; classIndex++) {
-
+    private double hAttribute() {
+        double entropy = 0.0;
+        for (int attributeIndex = 0; attributeIndex < evaluatedAttrDistinctValues; attributeIndex++) {
+            entropy += attributeProbability[attributeIndex] * (Math.log(attributeProbability[attributeIndex]) / Math.log(logBase));
         }
+        return -entropy;
+
+    }
+
+    private double hClass() {
+        double entropy = 0.0;
+        for (int classIndex = 0; classIndex < classAttrDistinctValues; classIndex++) {
+            entropy += classProbability[classIndex] * Math.log(classProbability[classIndex]) / Math.log(logBase);
+        }
+        return -entropy;
+    }
+
+    private double hClassAttribute() {
+        double entropy = 0.0;
+        double tmpAttrProb;
+        double tmpConditionalEntropy;
+
+        for (int attributeIndex = 0; attributeIndex < evaluatedAttrDistinctValues; attributeIndex++) {
+            tmpConditionalEntropy = 0.0;
+            tmpAttrProb = attributeProbability[attributeIndex];
+            for (int classIndex = 0; classIndex < classAttrDistinctValues; classIndex++) {
+                double partialEntropy = conditionalClassProbability[classIndex][attributeIndex]
+                        * Math.log(conditionalClassProbability[classIndex][attributeIndex]) / Math.log(logBase);
+                tmpConditionalEntropy += Double.isNaN(-partialEntropy) ? 0 : partialEntropy;
+            }
+            entropy += tmpAttrProb * (-tmpConditionalEntropy == Double.NaN ? 0 : -tmpConditionalEntropy);
+        }
+        return entropy;
+    }
+
+    private double[][] calculateConditionalProbabilityForClasses() {
+        double[][] conditionalProbabilityForClasses = new double[classAttrDistinctValues][evaluatedAttrDistinctValues];
+        double[] attributesValuesTotal = new double[evaluatedAttrDistinctValues];
+        for (int classIndex = 0; classIndex < classAttrDistinctValues; classIndex++) {
+            for (int attributeIndex = 0; attributeIndex < evaluatedAttrDistinctValues; attributeIndex++) {
+                conditionalProbabilityForClasses[classIndex][attributeIndex] = classAttrFreqMatrix[classIndex][attributeIndex];
+                attributesValuesTotal[attributeIndex] += classAttrFreqMatrix[classIndex][attributeIndex];
+            }
+        }
+        for (int classIndex = 0; classIndex < classAttrDistinctValues; classIndex++) {
+            for (int attributeIndex = 0; attributeIndex < evaluatedAttrDistinctValues; attributeIndex++) {
+                conditionalProbabilityForClasses[classIndex][attributeIndex] = conditionalProbabilityForClasses[classIndex][attributeIndex]
+                        / attributesValuesTotal[attributeIndex];
+            }
+        }
+
+//        System.out.println("Conditional Classes prob: " + Arrays.deepToString(conditionalProbabilityForClasses));
+        return conditionalProbabilityForClasses;
     }
 
     private double[] calculateProbabilityForAttributes() {
@@ -55,6 +121,7 @@ public class GainRatioAttrEval {
             }
             probabilityForAttributes[attributeIndex] /= data.numInstances();
         }
+//        System.out.println("Attr prob: " + Arrays.toString(probabilityForAttributes));
         return probabilityForAttributes;
     }
 
@@ -66,18 +133,23 @@ public class GainRatioAttrEval {
             }
             probabilityForClasses[classIndex] /= data.numInstances();
         }
+//        System.out.println("Classes prob: " + Arrays.toString(probabilityForClasses));
         return probabilityForClasses;
 
     }
-
-//    public double gain() {
-//        return (hClass(classAttrFreqMatrix) - hClassAtribbute(classAttrFreqMatrix)) / hAttribute(classAttrFreqMatrix);
-//    }
 
 
     public void printData() {
         for (int i = data.numInstances() - 1; i >= 0; i--) {
             System.out.println(data.get(i).toString());
         }
+    }
+
+    @Override
+    public String toString() {
+        String toString = ("EvalAttr: " + evaluatedAttr.toString() + " ClassAttr: " + classAttr.toString() + " hAttr: "
+                + hAttribute() + "| hClass: " + hClass() + "| hClassAttr: " + hClassAttribute() + "|gain: "
+                + (hClass() - hClassAttribute()) / hAttribute());
+        return toString;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,24 @@ import weka.core.Instances;
 public class Main {
 
     public static void main(String[] args) {
-//        WekaSimpleTransformer simpleTransformer = new WekaSimpleTransformer();
-//        simpleTransformer.readARFFFile( "filepath");
-//        simpleTransformer.removeInstances();
-//        simpleTransformer.removeAttribute();
-//        simpleTransformer.printData();
-//        simpleTransformer.saveData("filepath");
         Instances data = ArffFileHandler.readARFFFile("D:\\workspace\\pwr\\mgr1\\pwr-weka\\data\\13168055L3_1.arff");
         Instances discretizedData = ArffFileHandler.discretizeNumericData(data);
-        GainRatioAttrEval evaluator = new GainRatioAttrEval(discretizedData, "wiek", "status_pozyczki");
-
-//        ArffFileHandler.saveData(evaluator.getData(),"D:\\workspace\\pwr\\mgr1\\pwr-weka\\data\\13168055L3_x.arff");
+        GainRatioAttrEval evaluator = new GainRatioAttrEval(discretizedData, "okres_pobieral_dochod", "status_pozyczki");
         System.out.println(evaluator.gain());
-
+        evaluator = new GainRatioAttrEval(discretizedData, "ktore_rolowanie", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "miesieczny_dochod_netto", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "okres_bedzie_pobieral_dochod", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "plec", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "wiek", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "rodzaj_zr_dochodu", "status_pozyczki");
+        System.out.println(evaluator.gain());
+        evaluator = new GainRatioAttrEval(discretizedData, "kwota_kredytu", "status_pozyczki");
+        System.out.println(evaluator.gain());
     }
 
 
